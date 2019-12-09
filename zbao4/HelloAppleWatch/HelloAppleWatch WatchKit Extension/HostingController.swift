235eb2736f63452aabce6b0af9966230467bc3b3
bzy0204 <bzy0204@163.com>
2019-12-09 17:11:30 +0800

    1. 创建新工程 2. 创建EmojiData，更新首页显示

---
+++ zbao4/HelloAppleWatch/HelloAppleWatch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by 鲍志轶 on 2019/12/9.
+//  Copyright © 2019 鲍志轶. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ zbao4/HelloAppleWatch/HelloAppleWatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by 鲍志轶 on 2019/12/9.
+//  Copyright © 2019 鲍志轶. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
