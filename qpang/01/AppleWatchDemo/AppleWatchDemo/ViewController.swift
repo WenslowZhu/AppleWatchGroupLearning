5035dce345e6eb20b78c5b28bca985463ab635f4
Qiyou Pang <qpang@ford.com>
2019-12-12 17:38:07 +0800

    add hello apple watch demo

---
+++ qpang/01/AppleWatchDemo/AppleWatchDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleWatchDemo
+//
+//  Created by mgc000005751 on 12/12/19.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
